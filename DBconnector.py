1995c1456133005111ec2d6804aaaf287bb7e3a9
Victoria Norton <vsyms613@gmail.com>
2015-01-18 20:08:06 -0800

    added v0.1 - updated to oop, added people

--- DBconnector.py
+++ DBconnector.py
@@ -3,51 +3,26 @@ __author__ = 'Victoria'
 import os
 import sqlite3
 
-db_filename = "yellowpagersdb"
 
-db_new = not os.path.exists(db_filename)
+class AddressBookDB(object):
+    db_name = "yellowpagersdb"
 
-# conn = sqlite3.connect(db_filename)
+    def __init__(self, db):
+        self.db_name = db
 
-with sqlite3.connect(db_filename) as conn:
-    cursor = conn.cursor()
+    def connect(self):
+        db_new = not os.path.exists(self.db_name)
 
-if db_new:
-    print 'Need to create a schema'
-else:
-    print 'Database already exists'
+        with sqlite3.connect(self.db_name) as conn:
+            conn.cursor()
 
-# need to make this so its dynamic
-def insertPeople():
-    conn.execute("insert into people(unique_id, fname, lname, addressbook_code) \
-            values(9, 'Mike', 'Brown', 'TST')")
+        if db_new:
+            return 'Need to create a schema'
+        else:
+            return 'Database already exists'
 
-def deletePeople():
-    conn.execute("delete from people where unique_id = 9")
-    conn.commit
-
-
-insertPeople()
-
-cursor = conn.execute("select unique_id, fname, lname, addressbook_code from people")
-
-for row in cursor:
-    print "unique_id = ", row[0]
-    print "fname = ", row[1]
-    print "lname = ", row[2]
-    print "addressbook_code = ", row[3], "\n"
-
-deletePeople()
-
-print "Total number of rows deleted: ", conn.total_changes
-
-cursor = conn.execute("select unique_id, fname, lname, addressbook_code from people")
-
-for row in cursor:
-    print "unique_id = ", row[0]
-    print "fname = ", row[1]
-    print "lname = ", row[2]
-    print "addressbook_code = ", row[3], "\n"
-
-print "finished successfully!"
-conn.close()
+    def close_conn(self):
+        with sqlite3.connect(self.db_name) as conn:
+                conn.cursor()
+        conn.commit()
+        conn.close()
